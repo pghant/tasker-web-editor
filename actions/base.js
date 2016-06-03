220298dbd8b6db4fcc9fde15e0244539fcb9b4fa
pghant <prasant.ghantasala@gmail.com>
2016-06-02 20:31:51 -0400

    addProfile action

--- actions/base.js
+++ actions/base.js
@@ -1,5 +1,9 @@
-import * as types from '../constants/ActionTypes';
+import * as types from "../constants/ActionTypes";
 
 export function addProject(name) {
   return { type: types.ADD_PROJECT, name };
+}
+
+export function addProfile(name) {
+  return { type: types.ADD_PROFILE, name };
 }
