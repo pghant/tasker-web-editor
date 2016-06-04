85d77a2f3510264fb534a18551329fcff37d827b
pghant <prasant.ghantasala@gmail.com>
2016-06-03 22:51:17 -0400

    changed to ListItem

--- containers/ProjectList.jsx
+++ containers/ProjectList.jsx
@@ -1,12 +1,14 @@
 import React, { PropTypes, Component } from "react";
 import { connect } from "react-redux";
 import Drawer from "material-ui/Drawer";
-import MenuItem from "material-ui/MenuItem";
 import Subheader from "material-ui/Subheader";
 import FlatButton from "material-ui/FlatButton";
 import TextField from "material-ui/TextField";
+import { List, ListItem } from "material-ui/List";
+import ActionAdd from "material-ui/svg-icons/content/add";
+import IconButton from "material-ui/IconButton";
 import Dialog from "material-ui/Dialog";
-import { List, Map } from "immutable";
+import { List as ImmList, Map } from "immutable";
 import { addProject } from "../actions/base";
 
 class ProjectList extends Component {
@@ -36,17 +38,21 @@ class ProjectList extends Component {
     return (
       <div>
         <Drawer containerStyle={{marginTop: 65}}>
-          <Subheader>Projects</Subheader>
-          {
-            this.props.projects.map(project =>
-              <MenuItem key={project.get("id")}>{project.get("name")}</MenuItem>
-            )
-          }
-          <div style={{width: "100%"}}>
-            <div style={{margin: "0 auto", width: "50%"}}>
-              <FlatButton label="Add Project" primary={true} onTouchTap={this.handleOpen}/>
+          <List>
+            <Subheader>
+              Projects
+            </Subheader>
+            {
+              this.props.projects.map(project =>
+                <ListItem key={project.get("id")}>{project.get("name")}</ListItem>
+              )
+            }
+            <div style={{width: "100%"}}>
+              <div style={{margin: "0 auto", width: "50%"}}>
+                <FlatButton label="Add Project" primary={true} onTouchTap={this.handleOpen}/>
+              </div>
             </div>
-          </div>
+          </List>
         </Drawer>
         <Dialog title="Add New Project" modal={false} actions={dialogActions} open={this.state.dialogOpen} onRequestClose={this.handleClose}>
           <TextField
@@ -62,7 +68,7 @@ class ProjectList extends Component {
 }
 
 ProjectList.propTypes = {
-  projects: PropTypes.instanceOf(List).isRequired,
+  projects: PropTypes.instanceOf(ImmList).isRequired,
   onAddProject: PropTypes.func.isRequired
 };
 
