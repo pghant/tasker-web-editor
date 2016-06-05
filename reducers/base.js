52e83ee1a2f869540aaac65e737dd17ba8a71889
pghant <prasant.ghantasala@gmail.com>
2016-06-05 13:36:08 -0400

    Add pids and tids to projects in state Modify profile reducer to save for current project

--- reducers/base.js
+++ reducers/base.js
@@ -3,23 +3,27 @@ import { Map, List, fromJS } from "immutable";
 
 const initialState = fromJS({
   projectsById: {
-    0: { id: 0, name: "Base" }
+    0: { id: 0, name: "Base", tids: [], pids: [] }
   },
   projects: [0],
   profilesById: {},
   profiles: [],
   tasksById: {},
-  tasks: []
+  tasks: [],
+  ui: {
+    selectedProject: 0
+  }
 });
 
 export default function base(state = initialState, action) {
   let newId;
+  let selectedProject = state.getIn(["ui", "selectedProject"]);
   switch (action.type) {
     case ADD_PROJECT:
       newId = state.get("projects").max() + 1;
       return state.mergeDeep({
         projectsById: {
-          [newId]: { id: newId, name: action.name }
+          [newId]: { id: newId, name: action.name, tids: [], pids: [] }
         },
         projects: state.get("projects").push(newId)
       });
@@ -29,7 +33,12 @@ export default function base(state = initialState, action) {
         profilesById: {
           [newId]: { id: newId, name: action.name }
         },
-        profiles: state.get("profiles").push(newId)
+        profiles: state.get("profiles").push(newId),
+        projectsById: {
+          [selectedProject]: {
+            pids: state.getIn(["projectsById", `${selectedProject}`, "pids"]).push(newId)
+          }
+        }
       });
     case ADD_TASK:
       newId = state.get("tasks").count() === 0 ? 0 : state.get("tasks").max() + 1;
